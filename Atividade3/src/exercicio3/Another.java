5f96c337b7e07d04ecf951df6dd33cf62dca138d
gusDevel0per <127908396+gusDevel0per@users.noreply.github.com>
2023-09-02 18:24:13 -0300

    Add files via upload

--- Atividade3/src/exercicio3/Another.java
+++ Atividade3/src/exercicio3/Another.java
@@ -8,24 +8,24 @@ public class Another {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		String nomeRefeicao;
-		int numeroMesa, i;
+		int numeroMesa;
 		double valor, valorTotal;
-		int quantidade; 
-		
-		System.out.println("Numero da mesa: ");
-		numeroMesa = sc.nextInt();
-		
+		int quantidade;
 		System.out.println("Insira a bebida/refeição: ");
 		nomeRefeicao = sc.nextLine();
+		System.out.println("Numero da mesa: ");
+		numeroMesa = sc.nextInt();
+
 		System.out.println("Insira o valor: ");
-		valor = sc.nextDouble();		
+		valor = sc.nextDouble();
 		System.out.println("Insira a quantidade");
 		quantidade = sc.nextInt();
 		sc.close();
-		
+
 		valorTotal = valor * quantidade;
-		
-		System.out.printf("Mesa: %d\nPedido: %s\nQuantidade: %d\nValor total: %f", numeroMesa, nomeRefeicao, quantidade, valorTotal);
+
+		System.out.printf("Mesa: %d\nPedido: %s\nQuantidade: %d\nValor total: %.2f", numeroMesa, nomeRefeicao, quantidade,
+				valorTotal);
 	}
 
 }
